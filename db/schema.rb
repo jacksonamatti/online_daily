22204b135068805155a0c8b45f74dcd35d19a2a5
jackson amatti <amatti.tec@gmail.com>
2023-09-24 11:00:22 -0300

    style: add style in componets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_122752) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_131251) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_122752) do
     t.string "password_digest"
   end
 
+  add_foreign_key "posts", "users"
 end
